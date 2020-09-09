5af6ff0c767594d2918897b09a458995f8fb5438
Michael Chirico <MichaelChirico4@gmail.com>
2020-09-09 00:16:10 -0400

    more work on expression splitting

--- R/parsec.R
+++ R/parsec.R
@@ -2,35 +2,33 @@ parsec = function(file) {
   src = strsplit(readChar(file, file.info(file)$size), NULL)[[1L]]
   # logic below will assume terminal newline
   if (tail(src, 1L) != '\n') src = c(src, '\n')
-  src = strip_comments(src)
+  src = skip_white(preprocess(src))
+
+  n_char = length(src)
 
   # (i think) conservative guess of 40 characters/expression up front
-  exprs = vector('list', length(src) %/% 40L)
+  exprs = vector('list', n_char %/% 40L)
   expr_i = char_i = 1L
 
-  # define locally to have src in parent frame
-  skip_white = function(i) {
-    while (src[i] %in% c(' ', '\t', '\n')) {i = i+1L}
-    char_i <<- i
-    return(invisible())
-  }
-  skip_comment = function(i) {
-    if (src[i] == '/' && src[i+1L] == '/') {
-      i = i + 2L
-      while (src[i] != '\n') {i = i+1L}
-    } else if (src[i] == '/' && src[i+1L] == '*') {
-      i = i + 2L
-      while (src[i] != '*' || src[i+1L] != '/') {i = i+1L}
-    }
-    char_i <<- i
-  }
+  # strip out top-level expressions
   while (char_i <= length(src)) {
-    skip_white(char_i)
+    if (char_i > n_char) break
+    # pre-processor directives
+    if (src[char_i] == '#') {
+      end_expr = find_end_directive(src, char_i)
+      exprs[[expr_i]] = src[char_i:end_expr]
+      char_i = end_expr+1L
 
+    }
+    char_i = skip_white(char_i)
   }
 }
 
-strip_comments = function(txt) {
+# three preprocessing steps to normalize the code:
+#   (1) collapse lines linked with a continuation (\)
+#   (2) remove in-line comments (from // to a newline) and
+#   (3) remove block comments (delimited by /* ... */)
+preprocess = function(txt) {
   remaining = length(txt)
   i = 1L
   while (i <= remaining) {
@@ -44,6 +42,17 @@ strip_comments = function(txt) {
       while (txt[j] != '*' || txt[j+1L] != '/') { j=j+1L }
       txt = c(txt[1:(i-1L)], txt[j:remaining])
       remaining = remaining - j + i
+    } else if (txt[i] == '"' || txt[i] == "'") {
+      # bump ourselves outside of char/char array literals so
+      #   we're sure any other \ we find is for line continuation
+      delim = txt[i]
+      i = i+1L
+      while (txt[i] != delim && txt[i-1L] != '\\') { i=i+1L }
+    } else if (txt[i] == '\\') {
+      # overwrite from line continuation to newline with blanks
+      j = i+1L
+      while (txt[j] != '\n') { j=j+1L }
+      txt[i:j] = ''
     } else {
       i = i+1L
     }
@@ -51,9 +60,19 @@ strip_comments = function(txt) {
   txt
 }
 
-skip_white = function(
+# move the "cursor" along until non-whitespace is found.
+#   intended to be used at the beginning of an expression
+skip_white = function(txt, i) {
+  n = length(txt)
+  while (i <= n && txt[i] %in% c(' ', '\t', '\n')) {i = i+1L}
+  return(i)
+}
 
-# starting from first position of input txt, split it into expressions
-expr_split = function(txt) {
-
+#
+# initial position is at the # of a preprocessor directive. to find the end:
+#   - TODO: for ifdef, ifndef, if, there could be nesting of directives
+#   - for all others, end at the next newline (continuations are already removed)
+find_end_directive = function(txt, i) {
+  while (txt[i] != '\n') { i = i+1L }
+  return( i-1L )
 }
