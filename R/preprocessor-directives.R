d7eecfa376f166b2c5a4192b412d83b6dee651d3
Michael Chirico <MichaelChirico4@gmail.com>
2020-09-21 22:18:16 -0400

    note about dragons

--- R/preprocessor-directives.R
+++ R/preprocessor-directives.R
@@ -8,22 +8,37 @@ parse_directive = function(txt) {
   directive = paste(txt[char_i:end_directive], collapse='')
   switch(
     directive,
-    include = {
-      char_i = skip_white(txt, end_directive+1L)
-      end_arg = switch(
-        txt[char_i],
-        '<' = skip_pair_delim(txt, char_i, '<'),
-        '"' = skip_quoted(txt, char_i, '"')-1L,
-        stop(domain=NA, gettextf(
-          "Improper usage of #include directive: %s",
-          paste(txt, collapse=''), domain="R-parsec"
-        ))
-      )
-      call('directive_include', paste(txt[char_i:end_arg], collapse=''))
-    },
+    include = parse_include(txt, skip_white(txt, end_directive+1L)),
     define = {
-
     },
     stop("Unknown preprocessor directive")
   )
+}
+
+parse_include = function(txt, i) {
+  end_arg = switch(
+    txt[i],
+    '<' = skip_pair_delim(txt, i, '<'),
+    '"' = skip_quoted(txt, i, '"')-1L,
+    stop(domain=NA, gettextf(
+      "Improper usage of #include directive: %s",
+      paste(txt, collapse=''), domain="R-parsec"
+    ))
+  )
+  call('directive_include', paste(txt[i:end_arg], collapse=''))
+}
+
+# TODO: this is probably gonna cause issues -- unexpanded macros
+#    can generally lead to "non-syntactic" C-code which parses
+#    only after the macros are expanded. Substantially complicates
+#    the task of making the C AST. Tabled for later. Perhaps
+#    need an argument to parsec: `expand_macros`? With four
+#    values: "no", "local", "project", "recursive" -- to allow
+#    expansion of macros (1) from the same file (2) from
+#    headers in the same project (3) across all #includes etc
+# See: https://gcc.gnu.org/onlinedocs/cpp/Macro-Arguments.html#Macro-Arguments
+parse_define_object = function(txt, i) {
+}
+
+parse_define_function = function(txt, i) {
 }
